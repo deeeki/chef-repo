902a1624ba726cd715403db57c950bd3a97d537b
deeeki <deeeki@gmail.com>
2014-03-21 22:37:51 +0900

    Use swapfile in vagrant machines

--- site-cookbooks/vagrant/recipes/default.rb
+++ site-cookbooks/vagrant/recipes/default.rb
@@ -6,3 +6,23 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+bash 'create swapfile' do
+  user 'root'
+  code <<-EOC
+    dd if=/dev/zero of=/swapfile bs=1M count=2048 &&
+    chmod 600 /swapfile
+    mkswap /swapfile
+  EOC
+  only_if 'test ! -f /swapfile -a `cat /proc/swaps | wc -l` -eq 1'
+end
+
+mount '/dev/null' do
+  action :enable
+  device '/swapfile'
+  fstype 'swap'
+end
+
+bash 'activate swap' do
+  code 'swapon -ae'
+  only_if 'test `cat /proc/swaps | wc -l` -eq 1'
+end
