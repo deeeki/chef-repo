0c9e7086fbe8b70d1146e51e00d7ea4db41f21b0
deeeki <deeeki@gmail.com>
2014-03-21 22:44:50 +0900

    Recipe for MySQL5.6

--- site-cookbooks/mysql/recipes/default.rb
+++ site-cookbooks/mysql/recipes/default.rb
@@ -6,3 +6,24 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+remote_file "#{Chef::Config[:file_cache_path]}/mysql-community-release-el6-5.noarch.rpm" do
+  source 'http://repo.mysql.com/mysql-community-release-el6-5.noarch.rpm'
+  action :create
+end
+
+rpm_package 'mysql-community-release' do
+  source "#{Chef::Config[:file_cache_path]}/mysql-community-release-el6-5.noarch.rpm"
+  action :install
+end
+
+yum_package 'mysql-community-server' do
+  action :install
+end
+
+yum_package 'mysql-community-devel' do
+  action :install
+end
+
+service 'mysqld' do
+  action [:start, :enable]
+end
