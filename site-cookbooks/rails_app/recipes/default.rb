4759f24f80f7c429f5d46f498d4ea221aaf338f2
deeeki <deeeki@gmail.com>
2014-03-23 00:29:39 +0900

    Create deploy user and directory

--- site-cookbooks/rails_app/recipes/default.rb
+++ site-cookbooks/rails_app/recipes/default.rb
@@ -6,3 +6,23 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+user 'deploy' do
+  uid 1000
+  action :create
+end
+
+group 'deploy' do
+  members node.app.deploy_users
+  action :modify
+end
+
+directory '/var/www' do
+  action :create
+end
+
+directory "/var/www/#{node.app.name}" do
+  owner 'deploy'
+  group 'deploy'
+  mode '2775'
+  action :create
+end
