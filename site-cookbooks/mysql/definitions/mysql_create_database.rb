682bf1bbdf5cc52fb3f829a2d55c6713524a87b5
deeeki <deeeki@gmail.com>
2014-04-18 19:40:12 +0900

    Support mysql password

--- site-cookbooks/mysql/definitions/mysql_create_database.rb
+++ site-cookbooks/mysql/definitions/mysql_create_database.rb
@@ -1,6 +1,7 @@
 define :mysql_create_database, charset: 'utf8' do
   execute "mysql_create_db #{params[:name]}" do
-    command "mysql -u root -e 'CREATE DATABASE `#{params[:name]}` DEFAULT CHARACTER SET `#{params[:charset]}`;'"
-    not_if "/usr/bin/mysql -u root -D #{params[:name]}"
+    password_option = node.mysql.server_root_password != "" ? "-p#{node.mysql.server_root_password}" : ''
+    command "mysql -u root #{password_option} -e 'CREATE DATABASE `#{params[:name]}` DEFAULT CHARACTER SET `#{params[:charset]}`;'"
+    not_if "mysql -u root #{password_option} -D #{params[:name]}"
   end
 end
