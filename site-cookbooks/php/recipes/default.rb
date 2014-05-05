0bc75666d884cab35a2e41c0c9551db97d04c9a5
deeeki <deeeki@gmail.com>
2014-05-05 23:59:13 +0900

    Create recipe for PHP5.5

--- site-cookbooks/php/recipes/default.rb
+++ site-cookbooks/php/recipes/default.rb
@@ -6,3 +6,23 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+remote_file "#{Chef::Config[:file_cache_path]}/remi-release-6.rpm" do
+  source 'http://rpms.famillecollet.com/enterprise/remi-release-6.rpm'
+  action :create
+end
+
+rpm_package 'remi-release' do
+  source "#{Chef::Config[:file_cache_path]}/remi-release-6.rpm"
+  action :install
+end
+
+%w[php-devel php-mbstring php-fpm php-mysqlnd].each do |package|
+  yum_package package do
+    action :install
+    options '--enablerepo=remi-php55'
+  end
+end
+
+template '/etc/php.ini' do
+  source 'php.ini.erb'
+end
